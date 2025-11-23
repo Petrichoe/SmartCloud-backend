65676ea234eb003e6b746b5836a35496c0394dd1
Kevin <hello@itcast.cn>
2025-11-23 14:08:08 +0800

    领取优惠卷业务实现完毕

---
+++ tj-promotion/src/main/java/com/tianji/promotion/service/IUserCouponService.java
@@ -0,0 +1,26 @@
+package com.tianji.promotion.service;
+
+import com.tianji.promotion.domain.po.Coupon;
+import com.tianji.promotion.domain.po.UserCoupon;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * <p>
+ * 用户领取优惠券的记录，是真正使用的优惠券信息 服务类
+ * </p>
+ *
+ * @author kevin
+ * @since 2025-11-19
+ */
+public interface IUserCouponService extends IService<UserCoupon> {
+
+    void getUserCoupon(Long id);
+
+
+    void checkAndCreateUserCoupon(Coupon coupon, Long userId, Integer serialNum);
+
+    void exchangeCoupon(String code);
+
+
+}
